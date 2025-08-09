4c4995e2021179b1ca6cbe73702a7a4cf198a45b
JacquesCartier2 <20jackson03@gmail.com>
2025-08-09 13:07:24 -0400

    reorganized repository

--- src/main/java/com/chatservice/chatbot_service/constants/AppConstants.java
+++
@@ -1,5 +0,0 @@
-package com.chatservice.chatbot_service.constants;
-
-public class AppConstants {
-    public static final String chatbotHello = "You have successfully connected to the chatbot service, but you must format your request as a JSON POST, storing your message as 'prompt'.";
-}
