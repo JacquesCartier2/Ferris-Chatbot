c04ace9523111e32076f5425d73d446f4f66f4d5
JacquesCartier2 <20jackson03@gmail.com>
2025-08-14 11:53:01 -0400

    updated chatbotHello constant.

--- web_service/src/main/java/com/chatservice/chatbot_service/constants/AppConstants.java
+++ web_service/src/main/java/com/chatservice/chatbot_service/constants/AppConstants.java
@@ -1,5 +1,5 @@
 package com.chatservice.chatbot_service.constants;
 
 public class AppConstants {
-    public static final String chatbotHello = "You have successfully connected to the chatbot service, but you must format your request as a JSON POST, storing your message as 'prompt'.";
+    public static final String chatbotHello = "Hello, this is the chatbot web service!";
 }
